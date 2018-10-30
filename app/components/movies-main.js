5fcd95943bd21dcca47c11ef27008921b62d123c
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2018-10-30 23:23:21 +0100

    render popular movies

--- app/components/movies-main.js
+++ app/components/movies-main.js
@@ -1,4 +1,28 @@
+import $ from 'jquery';
 import Component from '@ember/component';
 
 export default Component.extend({
+
+	movies: [],
+
+  	didInsertElement: function() {
+    	this.getTopMovies();
+  	},
+
+  	getTopMovies: function() {
+  		let self = this;
+
+	    $.ajax({
+	      type: 'GET',
+	      url: 'https://movies-be.herokuapp.com/movies/top',
+	      async: true,
+	      success: function(topMovies) {
+	        $('#mm-loading-spinner').fadeOut(500, function() {
+	          self.get('movies').pushObjects(topMovies);
+	          $('#mm-movies').animate({opacity: 1}, 500);
+	        });
+	      }
+	    });
+  	}
+
 });
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('movie', {path: '/movie/:movieId'});
 });
 
 export default Router;
