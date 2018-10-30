dc495b29f5e8343c087d115a6864558f49ddcbe7
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2018-10-31 00:05:05 +0100

    implement top/popular movies toggle

--- app/components/movies-main.js
+++ app/components/movies-main.js
@@ -6,6 +6,7 @@ export default Component.extend({
 	movies: [],
 
   	didInsertElement: function() {
+  		this.setupListeners();
     	this.getTopMovies();
   	},
 
@@ -18,11 +19,41 @@ export default Component.extend({
 	      async: true,
 	      success: function(topMovies) {
 	        $('#mm-loading-spinner').fadeOut(500, function() {
-	          self.get('movies').pushObjects(topMovies);
-	          $('#mm-movies').animate({opacity: 1}, 500);
+	        	$('#mm-header').text('Top Movies');
+	        	$('.mm-movie-link').remove();
+	          	self.get('movies').pushObjects(topMovies);
+	          	$('#mm-movies').animate({opacity: 1}, 500);
 	        });
 	      }
 	    });
+  	},
+
+  	getPopularMovies: function() {
+  		let self = this;
+
+	    $.ajax({
+	      type: 'GET',
+	      url: 'https://movies-be.herokuapp.com/movies/popular',
+	      async: true,
+	      success: function(popularMovies) {
+	        $('#mm-loading-spinner').fadeOut(500, function() {
+	        	$('#mm-header').text('Popular Movies');
+	        	$('.mm-movie-link').remove();
+	          	self.get('movies').pushObjects(popularMovies);
+	          	$('#mm-movies').animate({opacity: 1}, 500);
+	        });
+	      }
+	    });
+  	},
+
+  	setupListeners: function() {
+  		let self = this;
+
+  		$('#mm-sort').change(function(){
+  			let sort = $('#mm-sort').val();
+  			if (sort === 'top') self.getTopMovies();
+  			if (sort === 'popular') self.getPopularMovies();
+  		});
   	}
 
 });
