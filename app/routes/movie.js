24b747a06fec412ef2ed5f76a99eb6c3db85df9b
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2018-10-31 02:09:38 +0100

    implement trailer view

--- app/routes/movie.js
+++ app/routes/movie.js
@@ -1,4 +1,31 @@
+import $ from 'jquery';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+	model: function(parameters){
+        return {
+            movie: this.getMovie(parameters.movieId),
+            trailer: this.getMovieTrailer(parameters.movieId)
+        };
+    },
+
+    getMovie: function(movieId) {
+		return $.parseJSON(Ember.$.ajax({
+                type: 'GET',
+                url: 'https://movies-be.herokuapp.com/movies/' + movieId,
+                async: false
+            }).responseText
+        );
+    },
+
+    getMovieTrailer: function(movieId) {
+		return $.parseJSON(Ember.$.ajax({
+                type: 'GET',
+                url: 'https://movies-be.herokuapp.com/movies/' + movieId + '/trailer',
+                async: false
+            }).responseText
+        );
+    }
+
 });
